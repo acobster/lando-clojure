ed68dadc0ae8c609b4288536b4fae3ac521d9360
Coby Tamayo <acobster@gmail.com>
2020-01-28 22:53:13 -0800

    clearer tests

--- bin/test-docker-build.sh
+++ bin/test-docker-build.sh
@@ -15,5 +15,17 @@ $dir/docker-build.sh -t LOCAL-TEST
 # Test the cli-tools Docker image
 docker run -it acobster/lando-clojure:cli-tools clojure -h
 
+if [[ $? != 0 ]] ; then
+  echo 'failed to run cli-tools'
+  exit 1
+fi
+
 # Test the Leiningen Docker image
 docker run -it acobster/lando-clojure:LOCAL-TEST lein
+
+if [[ $? != 0 ]] ; then
+  echo 'failed to run lein via docker'
+  exit 1
+fi
+
+echo OK
