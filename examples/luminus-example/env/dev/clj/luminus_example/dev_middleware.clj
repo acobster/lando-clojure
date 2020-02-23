6dbdb7347e0e7d6413f7585c772db77f46714e74
Coby Tamayo <acobster@gmail.com>
2020-02-23 10:22:52 -0800

    start over w/ basic luminus example sans datomic

---
+++ examples/luminus-example/env/dev/clj/luminus_example/dev_middleware.clj
@@ -0,0 +1,11 @@
+(ns luminus-example.dev-middleware
+  (:require
+    [ring.middleware.reload :refer [wrap-reload]]
+    [selmer.middleware :refer [wrap-error-page]]
+    [prone.middleware :refer [wrap-exceptions]]))
+
+(defn wrap-dev [handler]
+  (-> handler
+      wrap-reload
+      wrap-error-page
+      (wrap-exceptions {:app-namespaces ['luminus-example]})))
---
+++ examples/luminus-example/env/dev/clj/luminus_example/env.clj
@@ -0,0 +1,15 @@
+(ns luminus-example.env
+  (:require
+    [selmer.parser :as parser]
+    [clojure.tools.logging :as log]
+    [luminus-example.dev-middleware :refer [wrap-dev]]))
+
+(def defaults
+  {:init
+   (fn []
+     (parser/cache-off!)
+     (log/info "\n-=[luminus-example started successfully using the development profile]=-"))
+   :stop
+   (fn []
+     (log/info "\n-=[luminus-example has shut down successfully]=-"))
+   :middleware wrap-dev})
